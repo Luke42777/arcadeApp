39b68263a245b2c3c886593c6ca9a04bd3aae510
Lukasz Komsta <lukaszkomsta@yahoo.com>
2022-09-10 19:12:12 +0100

    Setting pixels using SDL2

--- arcadeApp.cpp
+++ arcadeApp.cpp
@@ -6,6 +6,19 @@ using namespace std;
 const int SCREEN_WIDTH = 224;
 const int SCREEN_HEIGHT = 288;
 
+size_t GetIndex(SDL_Surface* noptrWindowSurface, int row, int column) {
+	return row * noptrWindowSurface->w + column;
+}
+void SetPixel(SDL_Surface* noptrWindowSurface, uint32_t color, int x, int y) {
+	SDL_LockSurface(noptrWindowSurface);
+
+	uint32_t * pixels = (uint32_t*)noptrWindowSurface->pixels;
+	size_t index = GetIndex(noptrWindowSurface, y, x);
+	pixels[index] = color;
+
+	SDL_UnlockSurface(noptrWindowSurface);
+}
+
 int main(int argc, char* argv[]) {
 
 	if (SDL_Init(SDL_INIT_VIDEO)) {
@@ -13,10 +26,17 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	SDL_Window* optrWindow = SDL_CreateWindow("Arcade", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+	SDL_Window * optrWindow = SDL_CreateWindow("Arcade", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
 	if (optrWindow == nullptr) {
 		cout << "Couldn't create window. Got error: " << SDL_GetError() << endl;
 	}
+
+	SDL_Surface* noptrSurface = SDL_GetWindowSurface(optrWindow);
+
+	uint32_t color = 0xFF0000;
+
+	SetPixel(noptrSurface, color, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+	SDL_UpdateWindowSurface(optrWindow);
 
 	SDL_Event sdlEvent;
 	bool running = true;
