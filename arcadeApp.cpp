41883ca25c55c7f4370f5fa86ee806d4a5468f3d
Lukasz Komsta <lukaszkomsta@yahoo.com>
2022-09-10 18:34:02 +0100

    working SDL

--- arcadeApp.cpp
+++ arcadeApp.cpp
@@ -1,14 +1,40 @@
 
+#include "SDL.h"
 #include <iostream>
-#include "Vec2D.h"
+using namespace std;
+
+const int SCREEN_WIDTH = 224;
+const int SCREEN_HEIGHT = 288;
+
+int main(int argc, char* argv[]) {
+
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		std::cout << "SDL_Init failed\n";
+		return 1;
+	}
+
+	SDL_Window* optrWindow = SDL_CreateWindow("Arcade", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+	if (optrWindow == nullptr) {
+		cout << "Couldn't create window. Got error: " << SDL_GetError() << endl;
+	}
+
+	SDL_Event sdlEvent;
+	bool running = true;
+
+	while (running) {
+		while (SDL_PollEvent(&sdlEvent)) {
+			switch (sdlEvent.type) {
+				case SDL_QUIT:
+				{
+					running = false;
+					break;
+				}
+			}
+		}
+	}
+	SDL_DestroyWindow(optrWindow);
+	SDL_Quit();
 
 
-int main(int argc, char** argv)
-{
-    Vec2D vec(1,2);
-
-    std::cout << vec;
-
-    return 0;
+	return 0;
 }
-
