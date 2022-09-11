612756e08e88f6bff257edf6a43993fc8c9d266d
Lukasz Komsta <lukaszkomsta@yahoo.com>
2022-09-11 19:25:32 +0100

    color class added and implemented to Graphics folder in project

--- arcadeApp.cpp
+++ arcadeApp.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
 
 #include "SDL.h"
-#include <iostream>
+#include "Color.h"
+
 using namespace std;
 
 const int SCREEN_WIDTH = 224;
@@ -9,12 +11,12 @@ const int SCREEN_HEIGHT = 288;
 size_t GetIndex(SDL_Surface* noptrWindowSurface, int row, int column) {
 	return row * noptrWindowSurface->w + column;
 }
-void SetPixel(SDL_Surface* noptrWindowSurface, uint32_t color, int x, int y) {
+void SetPixel(SDL_Surface* noptrWindowSurface, Color color, int x, int y) {
 	SDL_LockSurface(noptrWindowSurface);
 
 	uint32_t * pixels = (uint32_t*)noptrWindowSurface->pixels;
 	size_t index = GetIndex(noptrWindowSurface, y, x);
-	pixels[index] = color;
+	pixels[index] = color.GetPixelColor();
 
 	SDL_UnlockSurface(noptrWindowSurface);
 }
@@ -26,16 +28,22 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	SDL_Window * optrWindow = SDL_CreateWindow("Arcade", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+	SDL_Window* optrWindow = SDL_CreateWindow("Arcade", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
 	if (optrWindow == nullptr) {
 		cout << "Couldn't create window. Got error: " << SDL_GetError() << endl;
 	}
 
 	SDL_Surface* noptrSurface = SDL_GetWindowSurface(optrWindow);
 
-	uint32_t color = 0xFF0000;
+	
+	
+	SDL_PixelFormat* pixelFormat = noptrSurface->format;
+	
+	Color::InitColorFormat(pixelFormat);
+	
 
-	SetPixel(noptrSurface, color, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+	cout << SDL_GetPixelFormatName(pixelFormat->format);
+	SetPixel(noptrSurface, Color::Blue(), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
 	SDL_UpdateWindowSurface(optrWindow);
 
 	SDL_Event sdlEvent;
@@ -54,7 +62,6 @@ int main(int argc, char* argv[]) {
 	}
 	SDL_DestroyWindow(optrWindow);
 	SDL_Quit();
-
 
 	return 0;
 }
